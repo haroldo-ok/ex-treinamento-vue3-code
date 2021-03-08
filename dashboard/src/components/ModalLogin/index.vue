3602b59626979a0105f1889baa67f706505ca747
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-03-08 10:02:40 -0300

    Ex "Criando modal login (Parte 3)" do módulo 4.

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -69,13 +69,18 @@
 
 <script>
 import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
 import { useField } from 'vee-validate'
+import { useToast } from 'vue-toastification'
 import useModal from '../../hooks/useModal'
 import { validateEmptyAndLength3, validateEmptyAndEmail } from '../../utils/validators'
+import services from '../../services'
 
 export default {
   setup () {
+    const router = useRouter()
     const modal = useModal()
+    const toast = useToast()
 
     const {
       value: emailValue,
@@ -101,7 +106,40 @@ export default {
       }
     })
 
-    function handleSubmit () {
+    async function handleSubmit () {
+      try {
+        toast.clear()
+        state.isLoading = true
+        const { data, errors } = await services.auth.login({
+          email: state.email.value,
+          password: state.password.value
+        })
+
+        if (!errors) {
+          window.localStorage.setItem('token', data.token)
+          router.push({ name: 'Feedbacks' })
+          modal.close()
+          return
+        }
+
+        if (errors.status === 404) {
+          toast.error('E-mail não encontrado')
+        }
+        if (errors.status === 401) {
+          toast.error('E-mail/senha inválidos')
+        }
+        if (errors.status === 400) {
+          toast.error('Ocorreu um erro ao fazer o login')
+        }
+
+        state.isLoading = false
+      } catch (error) {
+        console.error('Erro ao logar: ', error)
+
+        state.isLoading = false
+        state.hasErrors = !!error
+        toast.error('Ocorreu um erro ao fazer o login')
+      }
     }
 
     return {
