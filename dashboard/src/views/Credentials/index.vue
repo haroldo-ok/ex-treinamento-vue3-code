c6f64e73e9ea52412fcebbea8cef45fed40723c3
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-03-09 08:58:13 -0300

    "Criando tela de credenciais (Parte 3)", módulo 4.

--- dashboard/src/views/Credentials/index.vue
+++ dashboard/src/views/Credentials/index.vue
@@ -22,12 +22,20 @@
       <p class="mt-10 text-lg text-gray-800 font-regular">
         Este aqui é a sua chave de api
       </p>
+
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
       <div
-        class="flex py-3 pl-5 mt-2 rounded items-center bg-brand-gray w-1/2"
+        v-else
+        class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2"
       >
         <span>{{ store.User.currentUser.apiKey }}</span>
 
-        <div class="flex ml-20 mr-1">
+        <div class="flex ml-20 mr-5">
           <icon
             name="copy"
             :color="brandColors.graydark"
@@ -46,8 +54,15 @@
       <p class="mt-5 text-lg text-gray-800 font-regular">
         Coloque o script abaixo no seu site para começar a receber feedbacks
       </p>
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
       <div
-        class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-2/3 overflow-x-scroll"
+        v-else
+        class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll"
       >
         <pre>&lt;script src="https://haroldo-ok-feedbacker-widget.netlify.app?api_key={{ store.User.currentUser.apiKey }}"&gt;&lt;/script&gt;</pre>
       </div>
@@ -56,17 +71,23 @@
 </template>
 
 <script>
+import { reactive } from 'vue'
 import HeaderLogged from '../../components/HeaderLogged'
+import ContentLoader from '../../components/ContentLoader'
 import Icon from '../../components/Icon'
 import { useStore } from '../../hooks/useStore'
 import palette from '../../../palette'
 
 export default {
-  components: { HeaderLogged, Icon },
+  components: { ContentLoader, HeaderLogged, Icon },
   setup () {
     const store = useStore()
+    const state = reactive({
+      isLoading: false
+    })
 
     return {
+      state,
       store,
       brandColors: palette.brand
     }
