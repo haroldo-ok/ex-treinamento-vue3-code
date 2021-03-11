6c8ade4ec6f22dc3bb0a9b2ac1541bfcd66aae06
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-03-11 14:21:01 -0300

    "Criando tela de feedbacks (Parte 2)" do módulo 4.

--- dashboard/src/hooks/useStore.js
+++ dashboard/src/hooks/useStore.js
@@ -2,7 +2,11 @@ import Store from '../store'
 
 export function useStore (module) {
   if (module) {
-    return Store[module]
+    const store = Store[module]
+    if (!store) {
+      throw new Error(`Store name "${module}" is invalid; available stores are ${Object.keys(Store)}`)
+    }
+    return store
   }
 
   return Store
