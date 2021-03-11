3b4521c3562fd25c4223a7af59007d26fe11640f
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-03-11 16:38:25 -0300

    "Criando tela de feedbacks (Parte 4)" do módulo 4.

--- dashboard/src/views/Feedbacks/index.vue
+++ dashboard/src/views/Feedbacks/index.vue
@@ -20,28 +20,95 @@
         </h1>
         <suspense>
           <template #default>
-            <filters-loading class="mt-8" />
-          </template>
-          <template #fallback>
             <filters
               class="mt-8 animate__animated animate__fadeIn animate__faster"
             />
           </template>
+          <template #fallback>
+            <filters-loading class="mt-8" />
+          </template>
         </suspense>
       </div>
       <div class="px-10 pt-20 col-span-3">
+        <p
+          v-if="state.hasErro"
+          class="text-lg text-center text-gray-800 font-regular">
+          Aconteceu um erro ao carregar os feedbacks. 😢
+        </p>
+        <p
+          v-if="!state.feedbacks.length && !state.isLoading"
+          class="text-lg text-center text-gray-800 font-regular">
+          Ainda nenhum feedback recebido. 😎
+        </p>
 
+        <feedback-card-loading v-if="state.isLoading" />
+        <feedback-card
+          v-else
+          v-for="(feedback, index) in state.feedbacks"
+          :key="feedback.id"
+          :is-opened="index === 0"
+          :feedback="feedback"
+          class="mb-8"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { onMounted, reactive } from 'vue'
 import Filters from './Filters'
 import FiltersLoading from './FiltersLoading'
 import HeaderLogged from '../../components/HeaderLogged'
+import FeedbackCard from '../../components/FeedbackCard'
+import FeedbackCardLoading from '../../components/FeedbackCardLoading'
+import services from '../../services'
 
 export default {
-  components: { HeaderLogged, Filters, FiltersLoading }
+  components: {
+    HeaderLogged,
+    Filters,
+    FiltersLoading,
+    FeedbackCard,
+    FeedbackCardLoading
+  },
+  setup () {
+    const state = reactive({
+      isLoading: false,
+      feedbacks: [],
+      currentFeedbackType: '',
+      pagination: {
+        limit: 5,
+        offset: 0
+      },
+      hasError: false
+    })
+
+    onMounted(() => {
+      fetchFeedbacks()
+    })
+
+    function handleErrors (error) {
+      state.hasError = !!error
+    }
+
+    async function fetchFeedbacks () {
+      try {
+        state.isLoading = true
+        const { data } = await services.feedbacks.getAll({
+          ...state.pagination,
+          type: state.currentFeedbackType
+        })
+
+        state.feedbacks = data.results
+        state.pagination = data.pagination
+        state.isLoading = false
+      } catch (error) {
+        handleErrors(error)
+      }
+    }
+
+    return { state }
+  }
 }
 </script>
